30e36d9c32079c2eaee30cf14f6cb920795326c0
Abdulhaq <abdulhaqcontact@gmail.com>
2024-05-26 00:16:33 -0700

    project

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Project;
+use Response;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     */
+    public function index()
+    {
+        $projects = Project::orderBy('id','desc')->get();
+        return view('projects.index')->with(compact('projects'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     */
+    public function store(Request $request)
+    {
+        return $request->all();
+        $data = $request->validate([
+            
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        $project = Project::create($data);
+
+        return Response::json($project);
+    }
+}
